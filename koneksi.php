2abbd3a20b06eaf43b9f66e57c6ea2967989373c
M Zuhri Ardiansyah <gendutrologi147@gmail.com>
2026-05-06 22:24:49 +0700

    connect mysql railway

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,15 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "dharma_bahari");
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
+$db   = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+$conn = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
+
 ?>

a3138b03edfaa7828064b957e6e3341abe81b887
M Zuhri Ardiansyah <gendutrologi147@gmail.com>
2026-05-07 13:47:23 +0700

    update website

--- Kelola_soal.php
+++ Kelola_soal.php
@@ -106,6 +106,7 @@ $username  = $_SESSION['username'] ?? '-';
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kelola Soal</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
